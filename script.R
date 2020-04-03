e4f033b4d90755a09a929d33fd20fc286c5e4f8a
lukas vinoelst <lukas.vinoelst@student.kuleuven.be>
2020-04-03 16:31:44 +0200

    PCA
    maken van pca objecten

--- script.R
+++ script.R
@@ -3,3 +3,13 @@
 
 
 wijn <- read.csv("wijn.csv", header=FALSE, sep = ",")
+summary(wijn)
+
+#Data schalen
+wijnScaled = scale(wijn[,2:13])
+
+#ongeschaalde PCA maken
+wijn.pca = pprcomp(wijn)
+
+#geschaalde PCA maken
+wijnScaled.pca = prcomp(wijnScaled,scale=TRUE)
