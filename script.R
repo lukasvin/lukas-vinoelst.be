b9256b105d7e44fea860bd6592de81eb4da6e1de
lukas vinoelst <lukas.vinoelst@student.kuleuven.be>
2020-04-06 15:02:10 +0200

    nog wat pca

--- script.R
+++ script.R
@@ -1,3 +1,4 @@
+##-------------------------------------------------------------------------
 #!change working directory!
 #Data Inladen
 
@@ -6,10 +7,19 @@ wijn <- read.csv("wijn.csv", header=FALSE, sep = ",")
 summary(wijn)
 
 #Data schalen
-wijnScaled = scale(wijn[,2:13])
+wijnScaled = scale(wijn[,2:14])
 
-#ongeschaalde PCA maken
-wijn.pca = pprcomp(wijn)
+##-------------------------------------------------------------------------
 
-#geschaalde PCA maken
+#geschaalde PCA maken [waarschijnlijk best aangezien geen eenheden gegeven worden]
 wijnScaled.pca = prcomp(wijnScaled,scale=TRUE)
+
+plot(wijnScaled.pca)
+summary(wijnScaled.pca)
+
+scaledRot = wijnScaled.pca$rotation
+#PC1 verklaart 36,2% van de data en met een grote PC1 komt een lagere hoeveelheid flavonoiden,fenolen,proteines en proanrhocyanidine relatief t.o.v. de andere variabelen overeen. [ interpretatie : hoeveelheid smaak in wijn/hoe zuur is de wijn ]
+#PC2 verklaart 19,2% van de data en met een grote PC2 komt een lagere tint en proteineconcentratie relatief t.o.v. de hogere kleurintensiteit en hoger suikergehalte van de druif overeen. [ interpretatie : ? ]
+#PC3 verklaart 11,1% van de data en met een grote PC3 komt een lager suikergehalte van de druif relatief t.o.v. het hogere asgehalte en de hogere alkaliteit overeen. [ interpretatie : ? ]
+
+scaledPred = predict(wijnScaled.pca)
