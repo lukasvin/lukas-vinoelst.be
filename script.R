dbe20c396a0a75c33eb10d243e60174a347c9e04
lukas vinoelst <lukas.vinoelst@student.kuleuven.be>
2020-04-11 15:44:20 +0200

    lda,qda en knn
    ook functie aer erin gedaan en een loop om de beste k te zoeken

--- script.R
+++ script.R
@@ -2,12 +2,18 @@
 #!change working directory!
 #Data Inladen
 
+library(MASS)
+library(class)
 
 wijn <- read.csv("wijn.csv", header=FALSE, sep = ",")
+attach(wijn)
 summary(wijn)
 
 #Data schalen
 wijnScaled = scale(wijn[,2:14]);
+#extra col met de druifsoorten
+wijnScaledSp = wijn;wijnScaledSp[,2:14] = wijnScaled;
+
 
 ##-------------------------------------------------------------------------
 
@@ -34,7 +40,47 @@ validate_train = indices[-test];
 validate = sample(validate_train,50);
 train = validate_train[-which(validate_train %in% validate)];
 
+#dit is mogelijks niet nodig
 wijn.train = wijn[train,]
 wijn.validate = wijn[validate,]
 wijn.test = wijn[test,]
 
+#functie voor error rate
+aer = function(y1,y2,conf=TRUE) {
+  confusion = table(y1,y2)
+  if (conf) {print(confusion)}
+  observaties = sum(confusion)
+  verkeerd = observaties-sum(diag(confusion))
+  verkeerd/observaties
+}
+
+##-----------------------------------------------------------------------
+#determinantmethode
+#lineair
+
+wijn.lda <- lda(V1 ~.,wijnScaledSp ,subset = train);
+lda.pred.val <- predict(wijn.lda, wijnScaledSp[validate,]);
+
+#quadratisch
+
+wijn.qda <- qda(V1 ~.,wijnScaledSp ,subset = train);
+qda.pred.val <- predict(wijn.qda, wijnScaledSp[validate,])
+
+##----------------------------------------------------------------------
+#k-nearest-neighboursmethode
+#zoek k met kleinste error rate
+
+max_k = 78;
+resultaten = cbind(ER.validate=NULL)
+klijst = 1:max_k
+
+for (k in klijst) {
+  resultaten = rbind(resultaten,cbind(
+    ER.validate=aer(V1[validate],knn(wijnScaledSp[train,], wijnScaledSp[validate,], V1[train], k),FALSE) 
+  ))
+}
+
+head(resultaten)
+
+plot(c(0,78),c(0,1),xlab="Number of neigbours",ylab="Error rate",type="n");
+matplot(1:78,resultaten,type='l',add=TRUE)
